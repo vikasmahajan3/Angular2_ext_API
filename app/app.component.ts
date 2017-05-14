6ff8f50dc45416b8a75a420b72c20dea45202010
vmahajan <vikasmahajan90@gmail.com>
2017-05-14 18:38:30 +0530

    artist component added

--- app/app.component.ts
+++ app/app.component.ts
@@ -13,7 +13,7 @@ import { ProfileService } from './components/profile/profile.service'
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="#">GitHub Profile</a>
+          <a class="navbar-brand" routerLink="/">External API</a>
         </div>
         <div id="navbar" class="collapse navbar-collapse">
           <ul class="nav navbar-nav">
--- app/app.routing.ts
+++ app/app.routing.ts
@@ -2,12 +2,17 @@ import { SpotifyComponent } from './components/spotify/spotify.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { Routes, RouterModule } from '@angular/router';
 import { ModuleWithProviders } from '@angular/core';
+import { ArtistComponent } from './components/artist/artist.component';
+
 const appRoutes: Routes = [
     {
         path: '', component: ProfileComponent
     },
     {
         path: 'spotify', component: SpotifyComponent
+    },
+    {
+        path: 'artist/:id', component: ArtistComponent
     }
 ];
 export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
